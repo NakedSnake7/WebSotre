13cbdb77d6942e301ee23b56d650cf5e05ea89ed
Quetzalcoatl33 <weedtlanmx@gmail.com>
2025-02-06 13:10:09 -0600

    Actualización de archivos, excepto application.properties

---
+++ src/main/resources/static/assets/js/main.js
@@ -0,0 +1,22 @@
+/*!
+=========================================================
+* FoodHut Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// aqui se importan los componentes por separado en uno solo 
+
+import "./carrito.js";
+import "./formulario.js";
+import "./modal.js";
+import "./whatsapp.js";
+import "./foodhut.js";
+
